0bbf7efdf7d7b292c0bb2994a4e7c0cc57e1a4b1
Sean Conner <spc@conman.org>
2011-04-23 21:53:55 -0400

    Added some convenience functions

--- src/hash.c
+++ src/hash.c
@@ -14,6 +14,27 @@
 
 /************************************************************************/
 
+static int hash_hexa(
+	lua_State  *const restrict L,
+	const char *restrict       data,
+	size_t                     size
+)
+{
+  luaL_Buffer buf;
+  char        hex[3];
+  
+  luaL_buffinit(L,&buf);
+  while(size--)
+  {
+    snprintf(hex,3,"%02X",(unsigned char)*data++);
+    luaL_addlstring(&buf,hex,2);
+  }
+  luaL_pushresult(&buf);
+  return 1;
+}
+
+/************************************************************************/
+
 static int hashlua_init(lua_State *const L)
 {
   const EVP_MD *m;
@@ -66,7 +87,9 @@ static int hashlua_final(lua_State *const L)
   unsigned char  hash[EVP_MAX_MD_SIZE];
   unsigned int   hashsize;
   
-  ctx = luaL_checkudata(L,1,HASH_TYPE);
+  ctx      = luaL_checkudata(L,1,HASH_TYPE);
+  hashsize = sizeof(hash);
+  
   EVP_DigestFinal(ctx,hash,&hashsize);
   lua_pushlstring(L,(char *)hash,hashsize);
   return 1;
@@ -74,24 +97,79 @@ static int hashlua_final(lua_State *const L)
 
 /************************************************************************/
 
-static int hashlua_hexa(lua_State *L)
+static int hashlua_finalhexa(lua_State *const L)
 {
-  luaL_Buffer  buf;
-  const char  *data;
-  size_t       size;
-  char         hex[3];
+  const char *data;
+  size_t      size;
+  
+  hashlua_final(L);
+  data = lua_tolstring(L,-1,&size);
+  return hash_hexa(L,data,size);
+}
+
+/************************************************************************/
+
+static int hashlua_sum(lua_State *const L)
+{
+  const EVP_MD  *m;
+  EVP_MD_CTX     ctx;
+  const char    *data;
+  size_t         size;
+  const char    *alg;
+  unsigned char  hash[EVP_MAX_MD_SIZE];
+  unsigned int   hashsize;
   
   data = luaL_checklstring(L,1,&size);
-  luaL_buffinit(L,&buf);
   
-  while(size--)
+  if (size > INT_MAX)
   {
-    snprintf(hex,3,"%02X",(unsigned char)*data++);
-    luaL_addlstring(&buf,hex,2);
+    lua_pushnil(L);
+    lua_pushinteger(L,EFBIG);
+    return 2;
   }
   
-  luaL_pushresult(&buf);
+  alg  = luaL_optstring(L,2,"md5");
+  m    = EVP_get_digestbyname(alg);
+  if (m == NULL)
+  {
+    lua_pushnil(L);
+    lua_pushinteger(L,EINVAL);
+    return 2;
+  }
+  
+  hashsize = sizeof(hash);
+  EVP_DigestInit(&ctx,m);
+  EVP_DigestUpdate(&ctx,data,size);
+  EVP_DigestFinal(&ctx,hash,&hashsize);
+  lua_pushlstring(L,(char *)hash,hashsize);
   return 1;
+}
+
+/*****************************************************************/
+
+static int hashlua_hexa(lua_State *const L)
+{
+  const char  *data;
+  size_t       size;
+  
+  data = luaL_checklstring(L,1,&size);
+  return hash_hexa(L,data,size);
+}
+
+/************************************************************************/
+
+static int hashlua_sumhexa(lua_State *const L)
+{
+  const char *data;
+  size_t      size;
+  int         rc;
+  
+  rc = hashlua_sum(L);
+  if (!lua_isstring(L,-1))
+    return rc;
+  
+  data = lua_tolstring(L,-1,&size);
+  return hash_hexa(L,data,size);  
 }
 
 /************************************************************************/
@@ -109,10 +187,12 @@ static int hashlua___tostring(lua_State *const L)
 
 static const struct luaL_Reg hashlua[] =
 {
-  { "init"	, hashlua_init		} ,
+  { "new"	, hashlua_init		} ,
   { "update"	, hashlua_update	} ,
   { "final"	, hashlua_final		} ,
+  { "sum"	, hashlua_sum		} ,
   { "hexa"	, hashlua_hexa		} ,
+  { "sumhexa"	, hashlua_sumhexa	} ,
   { NULL	, NULL			}
 };
 
@@ -120,6 +200,7 @@ static const struct luaL_Reg hashlua_meta[] =
 {
   { "update"		, hashlua_update	} ,
   { "final"		, hashlua_final		} ,
+  { "finalhexa"		, hashlua_finalhexa	} ,
   { "__tostring"	, hashlua___tostring 	} ,
   { NULL		, NULL			}
 };
