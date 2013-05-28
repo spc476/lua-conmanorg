4386766213972730b034990b76bccb50ef285006
Sean Conner <spc@conman.org>
2013-05-27 21:17:20 -0400

    Compiler issue--fix typo

--- src/net.c
+++ src/net.c
@@ -212,7 +212,7 @@ static const struct strint m_errors[] =
 
 static inline size_t	  Inet_addrlen	(sockaddr_all__t *const)                               __attribute__((nonnull));
 static inline socklen_t   Inet_len    	(sockaddr_all__t *const)                               __attribute__((nonnull));
-static inline socklen_t   Inet_lensa	(sockaddr_all__t *const)                               __attribute__((nonnull));
+static inline socklen_t   Inet_lensa	(struct sockaddr *const)                               __attribute__((nonnull));
 static inline int         Inet_port   	(sockaddr_all__t *const)                               __attribute__((nonnull));
 static inline void        Inet_setport	(sockaddr_all__t *const,const int)                     __attribute__((nonnull(1)));
 static inline void	  Inet_setportn	(sockaddr_all__t *const,const int)                     __attribute__((nonnull(1)));
