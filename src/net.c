f42f01cffb058bd07b7a112b169192a7b0c1cb3e
Sean Conner <spc@conman.org>
2013-06-15 17:59:25 -0400

    Comment the new TCP_FASTOPEN option

--- src/net.c
+++ src/net.c
@@ -826,6 +826,9 @@ static const struct sockoptions m_sockoptions[] =
   { "dontroute"		, SOL_SOCKET 	, 0		, SO_DONTROUTE		, SOPT_FLAG 	, true , true  } ,
   { "error"		, SOL_SOCKET 	, 0		, SO_ERROR		, SOPT_INT  	, true , false } ,
 #ifdef TCP_FASTOPEN
+  /*---------------------------------------------------
+  ; read http://lwn.net/Articles/508865/ for more info
+  ;----------------------------------------------------*/
   { "fastopen"		, SOL_TCP	, 0		, TCP_FASTOPEN		, SOPT_INT	, true , true  } ,
 #endif
   { "keepalive"		, SOL_SOCKET 	, 0		, SO_KEEPALIVE		, SOPT_FLAG 	, true , true  } ,
