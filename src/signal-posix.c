6b8af96277d3e550198b553f7dba47d4f5f8109c
Sean Conner <spc@conman.org>
2014-07-20 16:58:05 -0400

    Add license and some overview information.

--- src/signal-posix.c
+++ src/signal-posix.c
@@ -1,3 +1,46 @@
+/********************************************
+*
+* Copyright 2014 by Sean Conner.  All Rights Reserved.
+*
+* This library is free software; you can redistribute it and/or modify it
+* under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation; either version 3 of the License, or (at your
+* option) any later version.
+*
+* This library is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+* or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+* License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this library; if not, see <http://www.gnu.org/licenses/>.
+*
+* Comments, questions and criticisms can be sent to: sean@conman.org
+*
+* ==================================================================
+*
+* Module:	org.conman.signal	(POSIX)
+*
+* Desc:		POSIX interface to signal()/raise().
+*
+* Example:
+*
+
+        signal = require "org.conman.signal"
+        
+        signal.catch('term',function(sig)
+            signal.raise('int')
+            print(sig,"I can survive")
+          end,
+          signal.set('int')
+        )
+        
+        signal.catch('int',function(sig) print("interrupt") end)
+        
+        signal.raise('term')
+        
+*
+*********************************************************************/
 
 
 /* http://pubs.opengroup.org/onlinepubs/7908799/xsh/signal.h.html */
