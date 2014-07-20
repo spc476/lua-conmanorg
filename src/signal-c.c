004673f569328ba219f0f22ed3a228768d3c7e99
Sean Conner <spc@conman.org>
2014-07-20 18:53:49 -0400

    Bring ANSI signal interface more in line with the POSIX signal interface.
    It is also backward compatible with how I do things, so this may be nearly a
    drop in replacement for the existing org.conman.process.sig implementation,
    which is nice (well, execept for how I reference signals).

--- src/signal-c.c
+++ src/signal-c.c
@@ -21,21 +21,21 @@
 *
 * Module:	org.conman.signal
 *
-* Desc:		ANSI C interface to signal()/raise().  Given the baroque
-*		nature of signals, this is a pretty simplisitic interface,
-*		it pretty much just flags any caught signal, which can then
-*		be tested.
+* Desc:		ANSI C interface to signal()/raise().  
 *
 * Example:
 *
+	signal = require "signal"
 
-      signal = require "org.conman.signal"
-      signal.catch('term')
-      signal.raise('term')
-      if signal.caught('term') then
-        print("I will survive")
-      end
+	signal.catch('term')
+	signal.raise('term')
+
+	if signal.caught('term') then
+	  print("I will survive")
+	end
       
+	signal.catch('abrt',function(sig) print(sig,"I survived") end)
+	signal.raise('abrt')
 *
 *********************************************************************/
 
@@ -50,28 +50,117 @@
 
 /**********************************************************************
 *
-* No assumptions are being made about the actual SIG* values.  As such, we
-* will have a variable for each possible signal (as defined by C89) that
-* will be set in the signal handler.  There is also one overall signal flag
-* that will be set for every signal handled.
+* No assumptions are being made about the actual SIG* values (they could be
+* consecutive, they could not be.  That's why I have separate variagbles for
+* each signal.  Sure, it's a bit verbose, but it's a more portable approach.
 *
 ***********************************************************************/
 
-static volatile sig_atomic_t m_caught;
-static volatile sig_atomic_t m_caught_abrt;
-static volatile sig_atomic_t m_caught_fpe;
-static volatile sig_atomic_t m_caught_ill;
-static volatile sig_atomic_t m_caught_int;
-static volatile sig_atomic_t m_caught_segv;
-static volatile sig_atomic_t m_caught_term;
+  /* modified in signal handler, thus the volatile modifier */
+  
+static volatile int           m_hookcount;
+static volatile int           m_hookmask;
+static volatile lua_Hook      m_hook;
+static volatile sig_atomic_t  m_bam;
+static volatile sig_atomic_t  m_signal;
+static volatile sig_atomic_t  m_caught;
+static volatile sig_atomic_t  m_caught_abrt;
+static volatile sig_atomic_t  m_caught_fpe;
+static volatile sig_atomic_t  m_caught_ill;
+static volatile sig_atomic_t  m_caught_int;
+static volatile sig_atomic_t  m_caught_segv;
+static volatile sig_atomic_t  m_caught_term;
 
-/**********************************************************************/
+  /* set under more controlled circumstances */
+  
+static int                    m_ref_abrt = LUA_NOREF;
+static int                    m_ref_fpe  = LUA_NOREF;
+static int                    m_ref_ill  = LUA_NOREF;
+static int                    m_ref_int  = LUA_NOREF;
+static int                    m_ref_segv = LUA_NOREF;
+static int                    m_ref_term = LUA_NOREF;
+static lua_State             *m_L;
+
+/***************************************************************************
+*
+* The signal handler backend. We remove any Lua debug hooks (how we got here
+* in the first place) then figure out which signal was triggered.  If
+* there's a function reference, call the function.  After all signals have
+* been processed, restore any Lua debug hooks and continue.
+*
+****************************************************************************/ 
+
+static void luasigbackend(lua_State *L,lua_Debug *ar __attribute__((unused)))
+{
+  volatile sig_atomic_t *ps;
+  const char            *sig;
+  int                    ref;
+  
+  lua_sethook(L,NULL,0,0);
+  
+  while(m_signal)
+  {
+    m_signal = 0;
+    
+    if (m_caught_abrt)
+      ref = m_ref_abrt , sig = "abrt" , ps = &m_caught_abrt;
+    else if (m_caught_fpe)
+      ref = m_ref_fpe  , sig = "fpe"  , ps = &m_caught_fpe;
+    else if (m_caught_ill)
+      ref = m_ref_ill  , sig = "ill"  , ps = &m_caught_ill;
+    else if (m_caught_int)
+      ref = m_ref_int  , sig = "int"  , ps = &m_caught_int;
+    else if (m_caught_segv)
+      ref = m_ref_segv , sig = "segv" , ps = &m_caught_segv;
+    else if (m_caught_term)
+      ref = m_ref_term , sig = "term" , ps = &m_caught_term;
+    else
+      continue;
+    
+    if (ref != LUA_NOREF)
+    {
+      *ps = 0;
+      lua_pushinteger(L,ref);
+      lua_gettable(L,LUA_REGISTRYINDEX);
+      lua_pushstring(L,sig);
+      if (lua_pcall(L,1,0,0) != 0)
+        lua_error(L);
+    }
+  }
+  
+  lua_sethook(L,m_hook,m_hookmask,m_hookcount);
+  m_bam = 0;
+}
+
+/***************************************************************************
+*
+* If not already handling signals, save any Lua debug hooks, then set one so
+* that anything that happens will trap to the signal backend.  We then
+* re-establish the signal handler [1] and set a flag for which signal was
+* triggered.
+*
+* [1]	The signal() handler is reset upon delivery.  Because of this, there
+*	is a potential to lose a signal.  But what can you do with ANSI C
+*	signal handling?
+*
+****************************************************************************/
 
 static void signal_handler(int sig)
 {
-  signal(sig,signal_handler);
-  m_caught = sig;
+  if (!m_bam)
+  {
+    m_bam = 1;
+    m_hookcount = lua_gethookcount(m_L);
+    m_hookmask  = lua_gethookmask (m_L);
+    m_hook      = lua_gethook     (m_L);
+    
+    lua_sethook(m_L,luasigbackend,LUA_MASKCALL | LUA_MASKRET | LUA_MASKCOUNT,1);
+  }
   
+  signal(sig,signal_handler);
+
+  m_signal = m_caught = 1;
+
   switch(sig)
   {
     case SIGABRT: m_caught_abrt = 1; break;
@@ -84,7 +173,11 @@ static void signal_handler(int sig)
   }
 }
 
-/**********************************************************************/
+/**********************************************************************
+*
+* Function to map a string to a signal value.
+*
+***********************************************************************/
 
 struct mapstrint
 {
@@ -124,12 +217,10 @@ static int slua_tosignal(lua_State *const L,int idx)
     { "terminate"	, SIGTERM	} ,
   };
   
-# define MAXSIG (sizeof(sigs) / sizeof(struct mapstrint))
-
   struct mapstrint *entry = bsearch(
           luaL_checkstring(L,idx),
           sigs,
-          MAXSIG,
+          sizeof(sigs) / sizeof(struct mapstrint),
           sizeof(struct mapstrint),
           mapstrintcmp
         );
@@ -151,7 +242,7 @@ static int slua_tosignal(lua_State *const L,int idx)
 *
 * Input:	signal	(string/optional) name of signal
 * 
-* Return:	boolean
+* Return:	status (boolean) true if signal has been caught.
 *
 ***********************************************************************/
 
@@ -184,22 +275,60 @@ static int siglua_caught(lua_State *const L)
 
 /**********************************************************************
 *
-* Usage:	signal.catch(signal[,signal...])
+* Usage:	okay,err = signal.catch(signal[,handler])
 *
-* Desc:		Catches one or more signals.
+* Desc:		Catches one or more signals.  If no handler is given, set
+*		a flag that signal.caught() can check.
 *
 * Input:	signal (string) name of signal
+*		handler (function/optional) handler for signal.
+*
+* Return:	okay (boolean) true if successful, false if failed
+*		err (integer)  system error, 0 if successful
 *
 ***********************************************************************/		
 
 static int siglua_catch(lua_State *const L)
 {
-  int top = lua_gettop(L);
-  int i;
+  int sig;
+  int ref;
   
-  for (i = 1 ; i <= top ; i++)
-    signal(slua_tosignal(L,i),signal_handler);
-  return 0;
+  sig = slua_tosignal(L,1);
+  
+  switch(sig)
+  {
+    case SIGABRT: ref = m_ref_abrt; m_ref_abrt = LUA_NOREF; break;
+    case SIGFPE:  ref = m_ref_fpe;  m_ref_fpe  = LUA_NOREF; break;
+    case SIGILL:  ref = m_ref_ill;  m_ref_ill  = LUA_NOREF; break;
+    case SIGINT:  ref = m_ref_int;  m_ref_int  = LUA_NOREF; break;
+    case SIGSEGV: ref = m_ref_segv; m_ref_segv = LUA_NOREF; break;
+    case SIGTERM: ref = m_ref_term; m_ref_term = LUA_NOREF; break;
+    default:                                                break;
+  }
+  
+  luaL_unref(L,LUA_REGISTRYINDEX,ref);
+  
+  if (lua_isfunction(L,2))
+  {
+    lua_pushvalue(L,2);
+    ref = luaL_ref(L,LUA_REGISTRYINDEX);
+      
+    switch(sig)
+    {
+      case SIGABRT: m_ref_abrt = ref; break;
+      case SIGFPE:  m_ref_fpe  = ref; break;
+      case SIGILL:  m_ref_ill  = ref; break;
+      case SIGINT:  m_ref_int  = ref; break;
+      case SIGSEGV: m_ref_segv = ref; break;
+      case SIGTERM: m_ref_term = ref; break;
+      default:                        break;
+    } 
+  }
+  
+  signal(sig,signal_handler);
+  lua_pushboolean(L,1);
+  lua_pushinteger(L,0);
+  return 2;
 }
 
 /**********************************************************************
@@ -266,6 +395,22 @@ static int siglua_raise(lua_State *const L)
   return 2;
 }
 
+/**********************************************************************
+*
+* Usage:	implementation = signal.SIGNAL()
+*
+* Desc:		Return the implementation of this module.
+*
+* Return:	implementation (string) "ANSI"
+*
+**********************************************************************/
+
+static int siglua_SIGNAL(lua_State *const L)
+{
+  lua_pushliteral(L,"ANSI");
+  return 1;
+}
+
 /**********************************************************************/
 
 static const struct luaL_Reg m_sig_reg[] =
@@ -275,11 +420,13 @@ static const struct luaL_Reg m_sig_reg[] =
   { "ignore"	, siglua_ignore		} ,
   { "default"	, siglua_default	} ,
   { "raise"	, siglua_raise		} ,
+  { "SIGNAL"	, siglua_SIGNAL		} ,
   { NULL	, NULL			}
 };
 
 int luaopen_signal(lua_State *const L)
 {
+  m_L = L;
   luaL_register(L,"signal",m_sig_reg);
   return 1;
 }
