9a339a2d5e6b96745e852c461a00c3f343a7eb1b
Sean Conner <spc@conman.org>
2014-10-09 22:50:17 -0400

    A quick test of the clock.sleep() function.

---
+++ test/clock-test.lua
@@ -0,0 +1,27 @@
+
+local clock   = require "org.conman.clock"
+local process = require "org.conman.process"
+local signal  = require "org.conman.signal-posix"
+
+local child = process.fork()
+if not child then
+  io.stderr:write("failure\n")
+  os.exit(1)
+end
+
+if child == 0 then
+  signal.catch('int')
+  local left = clock.sleep(2)
+  if left >= 0.9 and left <= 1.0 then
+    process.exit(0)
+  else
+    proess.exit(1)
+  end
+else
+  clock.sleep(1)
+  signal.raise('int',child)
+  info = process.wait(child)
+end
+
+assert(info.rc == 0)
+  
