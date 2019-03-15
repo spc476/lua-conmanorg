0eaafcffcbbf14a6ba4d7065665fd45eee60d078
Sean Conner <spc@conman.org>
2019-03-14 22:01:34 -0400

    Update org.conman.debug.hexdump()
    API change, but it's better.  And shorter.  And cleaner.  And all around
    nicer.  Also, you can easier modify this to output to a file, or syslog(),
    or anything else really.

--- lua/debug.lua
+++ lua/debug.lua
@@ -20,13 +20,16 @@
 -- ********************************************************************
 -- luacheck: globals EDITOR edit editf hexdump
 -- luacheck: ignore 611
+
 local os       = require "os"
 local debug    = require "debug"
 local io       = require "io"
 local string   = require "string"
+local char     = require "org.conman.parsers.ascii.char"
+local lpeg     = require "lpeg"
+
 local _VERSION = _VERSION
 local dofile   = dofile
-
 local loadstring
 
 if _VERSION == "Lua 5.1" then
@@ -34,7 +37,7 @@ if _VERSION == "Lua 5.1" then
   module("org.conman.debug")
 else
   loadstring = load
-  _ENV = {}
+  _ENV       = {}
 end
 
 EDITOR = os.getenv("VISUAL") or os.getenv("EDITOR") or "/bin/vi"
@@ -144,33 +147,33 @@ end
 
 -- ********************************************************************
 
-function hexdump(t,f,bias)
-  bias         = bias or 0
-  local offset = 1
-  local out    = f or io.stdout
+local toascii = lpeg.Cs((char + lpeg.P(1) / ".")^0)
+local tohex   = lpeg.Cs((lpeg.P(1) / function(c)
+  return ("%02X "):format(c:byte())
+end)^0)
+
+function hexdump(bin,delta,bias,out)
+  delta = delta or 16
+  bias  = bias  or 0
+  out   = out   or function(off,hex,ascii,d)
+    io.stderr:write(
+        ("%08X: "):format(off),
+        hex,
+        (" "):rep(3 * (d - (#hex / 3))),
+        ascii,
+        "\n"
+    )
+  end
   
-  while true do
-    if offset > string.len(t) then return end
-    local s = string.sub(t,offset,offset + 15)
-    out:write(string.format("%08X: ",(offset - 1) + bias))
-    string.gsub(s,"(.)",
-            function (c) out:write(string.format("%02X ",string.byte(c))) end)
-    out:write(string.rep(" ",3*(16-string.len(s))))
+  local offset = 1
+  
+  while offset <= #bin do
+    local s = bin:sub(offset,offset + (delta - 1))
+    local h = tohex:match(s)
+    local a = toascii:match(s)
     
-    string.gsub(s,"(.)",
-            function (c)
-              local x = string.byte(c)
-              if x < 32 then
-                out:write(".")
-              elseif x > 127 then
-                out:write(".")
-              else
-                out:write(c)
-              end
-            end
-            )
-    out:write("\n")
-    offset = offset+16
+    out((offset - 1) + bias,h,a,delta)
+    offset = offset + delta
   end
 end
 
