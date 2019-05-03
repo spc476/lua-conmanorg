ddad09cda44eaf641bdb0601e85609e85ba20a16
Sean Conner <spc@conman.org>
2019-05-03 00:27:56 -0400

    Merge org.conman.net.reader into org.conman.net.ios

--- lua/net/ios.lua
+++ lua/net/ios.lua
@@ -24,12 +24,129 @@
 local errno  = require "org.conman.errno"
 local string = require "string"
 local table  = require "table"
-local READER = require "org.conman.net.reader"
+local lpeg   = require "lpeg"
 
 local select   = select
 local type     = type
 local error    = error
 local unpack   = table.unpack or unpack
+
+-- *******************************************************************
+
+local CRLF     = lpeg.P"\r"^-1 * lpeg.P"\n"
+local notCRLF  = lpeg.R("\0\9","\11\12","\14\255")
+
+	-- -------------------------------------------------------
+	-- The LPEG patterns LINE, LINEcrlf and HEADERS return the
+	-- following values:
+	--
+	--	poscap	- maximum position of capture
+	--	posres	- position to resume scanning
+	--	eol	- true for full capture, false if need more
+	-- --------------------------------------------------------
+
+local LINE     = notCRLF^0 * lpeg.Cp()
+               * (
+                   CRLF * lpeg.Cp() * lpeg.Cc(true)
+                        + lpeg.Cp() * lpeg.Cc(false)
+                 )
+local LINEcrlf = notCRLF^0
+               * (
+                   CRLF * lpeg.Cp() * lpeg.Cp() * lpeg.Cc(true)
+                        + lpeg.Cp() * lpeg.Cp() * lpeg.Cc(false)
+                 )
+local restq    = CRLF       * lpeg.Cp()       * lpeg.Cp() * lpeg.Cc(true)
+               + notCRLF^1  * lpeg.Cp()       * lpeg.Cp() * lpeg.Cc(false)
+               + lpeg.P(-1) * lpeg.Cb('here') * lpeg.Cp() * lpeg.Cc(false)
+local HEADERS  = CRLF * CRLF * lpeg.Cp() * lpeg.Cp() * lpeg.Cc(true)
+               + CRLF * (notCRLF^1 * lpeg.Cg(lpeg.Cp(),'here') * CRLF)^1 * restq
+               +        (notCRLF^1 * lpeg.Cg(lpeg.Cp(),'here') * CRLF)^1 * restq
+               +         notCRLF^1 * lpeg.P(-1) * lpeg.Cp() * lpeg.Cp() * lpeg.Cc(false)
+               +                                  lpeg.Cp() * lpeg.Cp() * lpeg.Cc(false)
+                              
+-- *******************************************************************
+-- usage:	data = read_data(ios,pattern)
+-- desc:	Read data from a source
+-- input:	ios (table) Input/Output object
+--		pattern (userdata/LPEG) one of LINE, LINEcrlf or HEADERS
+-- return:	data (string) data from data source
+-- *******************************************************************
+
+local function read_data(ios,pattern)
+  if ios._eof then
+    return nil
+  end
+  
+  local poscap,posres,eol = pattern:match(ios._readbuf,ios._rpos)
+  
+  if not eol then
+    local data = ios:_refill()
+    
+    if #data == 0 then
+      ios._eof = true
+      return ios._readbuf
+    end
+    
+    ios._rpos    = posres
+    ios._readbuf = ios._readbuf .. data
+    return read_data(ios,pattern)
+    
+  else
+    local data   = ios._readbuf:sub(1,poscap-1)
+    ios._readbuf = ios._readbuf:sub(posres,-1)
+    ios._rpos    = 1
+    return data
+  end
+end
+
+-- *******************************************************************
+
+local READER =
+{
+  ['*n'] = function()
+    error "Not implemented"
+  end,
+
+-- *******************************************************************
+
+  ['*l'] = function(ios)
+    return read_data(ios,LINE)
+  end,
+
+-- *******************************************************************
+
+  ['*L'] = function(ios)
+    return read_data(ios,LINEcrlf)
+  end,
+
+-- *******************************************************************
+
+  ['*a'] = function(ios)
+    if ios._eof then
+      return ""
+    end
+    
+    repeat
+      local data = ios:_refill()
+      ios._readbuf = ios._readbuf .. data
+    until #data == 0
+    
+    ios._eof = true
+    return ios._readbuf
+  end,
+
+-- *******************************************************************
+ 
+  ['*h'] = function(ios)
+    return read_data(ios,HEADERS)
+  end,
+}
+
+READER['a'] = READER['*a']
+READER['l'] = READER['*l']
+READER['n'] = READER['*n']
+READER['L'] = READER['*L']
+READER['h'] = READER['*h']
 
 -- *******************************************************************
 --
@@ -169,14 +286,18 @@ local function write(ios,...)
     return false,errno[errno.ECONNRESET],errno.ECONNRESET
   end
   
+  local output = ""
+  
   for i = 1 , select('#',...) do
     local data = select(i,...)
     if type(data) ~= 'string' and type(data) ~= 'number' then
       error("string or number expected, got " .. type(data))
     end
-    ios:_drain(data)
+    
+    output = output .. data
   end
   
+  ios:_drain(output)
   return true
 end
 
