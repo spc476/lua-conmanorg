5ffd5967800a592d68fd19daf3c93d3bb35f390e
Sean Conner <spc@conman.org>
2020-11-20 21:04:40 -0500

    Bug fix---schedule coroutine when no more data was missing

--- lua/nfl/tls.lua
+++ lua/nfl/tls.lua
@@ -154,6 +154,7 @@ local function create_handler(conn,remote)
         if #packet == 0 then
           nfl.SOCKETS:remove(conn)
           ios._eof = true
+          nfl.schedule(ios.__co,true)
         end
         
         ios.__rawbuf = ios.__rawbuf .. packet
