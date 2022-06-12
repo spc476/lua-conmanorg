58aaa634fc06031979c2bb437ab03fc7fc308f30
Sean Conner <spc@conman.org>
2022-06-11 22:41:10 -0400

    Bug fix---fix compiliation error on Mac OS-X

--- src/fsys.c
+++ src/fsys.c
@@ -39,7 +39,9 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/sysmacros.h>
+#if !defined(__APPLE__)
+#  include <sys/sysmacros.h>
+#endif
 #include <unistd.h>
 #include <fcntl.h>
 #include <dirent.h>
