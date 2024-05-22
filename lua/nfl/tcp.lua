ee01a8f97ceeb9a3dfd70e6ed0f165f6d6d2237b
Sean Conner <spc@conman.org>
2024-05-21 22:20:58 -0400

    Bug fix---potential fix to lower CPU usage for TCP streams
    The old code:
    
    	ios._drain = function(self,data)
    	  ios.__output = data
    	  nfl.SOCKETS:update(self.__socket,'w')
    	  return coroutine.yield()
    	end
    
    will cause a reschedule for all writes, and this appears to generate a fair
    load on the CPU.  Now, we just write the data to the kernel, and only switch
    when a partial write is done.  This should decrease the amount of CPU
    shuffling going on.

--- lua/nfl/tcp.lua
+++ lua/nfl/tcp.lua
@@ -62,9 +62,21 @@ local function create_handler(conn,remote)
   end
   
   ios._drain = function(self,data)
-    ios.__output = data
-    nfl.SOCKETS:update(self.__socket,'w')
-    return coroutine.yield()
+    local bytes,err = self.__socket:send(nil,data)
+    if err == 0 then
+      ios.__wbytes = ios.__wbytes + bytes
+      if bytes < #data then
+        ios.__output = data:sub(#data+1,-1)
+        nfl.SOCKETS:update(self.__socket,'w')
+        return coroutine.yield()
+      end
+      return true
+    else
+      syslog('error',"socket:send() = %s",errno[err])
+      nfl.SOCKETS:remove(ios.__socket)
+      ios._eof = true
+      return false,errno[err],err
+    end
   end
   
   ios.close = function(self)
