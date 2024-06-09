528c2a1787f43247f9d87c19209073191f44bafb
Sean Conner <spc@conman.org>
2024-06-09 17:49:53 -0400

    Remove unused function.

--- lua/table.lua
+++ lua/table.lua
@@ -18,7 +18,7 @@
 -- Comments, questions and criticisms can be sent to: sean@conman.org
 --
 -- ********************************************************************
--- luacheck: globals show safestring dump_value dump keepset
+-- luacheck: globals safestring dump_value dump keepset
 -- luacheck: ignore 611
 
 local _VERSION     = _VERSION
@@ -39,59 +39,6 @@ else
 end
 
 -- *******************************************************************
-
-function show(l,fout)
-  fout = fout or io.stdout
-  local maxkeylen = 0
-  local maxvallen = 0
-  
-  if type(l) ~= 'table' then
-    return
-  end
-  
-  local function conv_cntl(s)
-    if s == "\n" then
-      return "\\n"
-    else
-      return "\\?"
-    end
-  end
-  
-  for k,v in pairs(l)
-  do
-    maxkeylen = math.max(maxkeylen,string.len(tostring(k)))
-    maxvallen = math.max(maxvallen,string.len(string.format("%q",tostring(v))))
-  end
-  
-  ----------------------------------------------------
-  -- clip the value portion to both the key and the
-  -- value will display without wrapping in an 80
-  -- column screen.  Also, the Lua runtime does not
-  -- support formats longer than 99 characters wide.
-  -----------------------------------------------------
-  
-  maxkeylen = math.min(30,maxkeylen)
-  maxvallen = math.min(77 - maxkeylen,maxvallen)
-  
-  local format = string.format("%%-%ds %%-%ds",maxkeylen + 1,maxvallen + 1)
-  
-  for k,v in pairs(l)
-  do
-    local vt
-    
-    if type(v) == "string" then
-      vt = string.format("%q",v)
-    else
-      vt = tostring(v)
-    end
-    
-    vt = string.gsub(vt,"%c",conv_cntl)
-    vt = string.sub(vt,1,maxvallen)
-    fout:write(string.format(format,tostring(k),vt),"\n")
-  end
-end
-
--- *************************************************************
 
 local char_trans =
 {
